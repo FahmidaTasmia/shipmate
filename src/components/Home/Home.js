ed859a87f405b017f0ef4c71e14a0b92abbd5bcf
fahmida76 <63741081+fahmida76@users.noreply.github.com>
2023-04-26 18:15:58 +0600

    header part and part of home page done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,40 @@
 import React from 'react'
-
+import { CiLocationOn } from "react-icons/ci";
+import { GoCalendar } from "react-icons/go";
+import { RiCloudy2Line } from "react-icons/ri";
+import { BsArrowRight } from "react-icons/bs";
+import { Link } from 'react-router-dom';
 function Home() {
   return (
     <div>
-      
+      <div className='m-auto text-center my-[100px]'>
+        <h2 className='text-[42px] font-semibold leading-[50px] text-black mt-[100px]'>Hassle-Free Shipping Solutions</h2>
+        <p className='text-[22px] text-black font-[500] leading-[26px] pt-4'>Compare, book, and manage your freight across the world’s top logistics providers, all on one platform.</p>
+      </div>
+
+      <div className='w-[1287px] h-[82px] bg-white flex m-auto rounded-[14px] justify-between px-3'>
+        <div className='flex text-lg gap-1'>
+            <p className='self-center '><CiLocationOn></CiLocationOn></p>
+            <p className='self-center '> Origin,Port,City</p>
+        </div>
+        <div className='flex text-lg gap-1'>
+            <p className='self-center '><CiLocationOn></CiLocationOn></p>
+            <p className='self-center '>Destination,Port,City</p>
+        </div>
+
+        <div className='flex text-lg gap-2 '>
+            <p className='self-center border-l-2 pl-2'><GoCalendar></GoCalendar></p>
+            <p className='self-center '>13 April 2023</p>
+        </div>
+
+        <div className='flex text-lg gap-2 '>
+            <p className='self-center border-l-2 pl-2'><RiCloudy2Line></RiCloudy2Line></p>
+            <p className='self-center '>Load</p>
+        </div>
+        <div className='flex'>
+            <Link to='/' className='btn self-center border border-[#9747FF] hover:bg-transparent hover:text-[#9747FF] btn-sm  hover:border-[#9747FF] bg-[#9747FF]'><BsArrowRight className='font-bold '></BsArrowRight></Link>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Shared/Header.js
+++ src/components/Shared/Header.js
@@ -1,37 +1,75 @@
-import React from 'react'
+import React from 'react';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 
-function Header() {
-  return (
-    <div>
-      <div className="navbar bg-[#F8F9FD]">
-  <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-  </div>
-  <div className="flex-none gap-2">
-    <div className="form-control">
-      <input type="text" placeholder="Search" className="input input-bordered" />
-    </div>
-    <div className="dropdown dropdown-end">
-      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+import { AuthContext } from '../../contexts/AuthProvider';
+
+const Header = () => {
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.error(error))
+    }
+    const menuItems = <React.Fragment>
+    
+    <div className='flex gap-5'>
+              
+              <div  >
+                          {
+                              user?.uid?
+                              <>
+                             
+                              <button className='btn btn-sm px-10 bg-transparent text-black border border-[#6F57E9] normal-case hover:bg-[#6F57E9] hover:text-white' onClick={ handleLogOut} >$</button>
+                              </>
+                              :
+                              <>
+                              <Link className='btn btn-sm px-10 bg-[#6F57E9] border border-[#6F57E9] text-white hover:text-black hover:bg-transparent hover:border-[#6F57E9] normal-case  tracking-wider' to='/login'>Login</Link>
+                              
+                              </>
+                          }
+                          </div>
+      
+                          <div>
+      
+                          {
+                              user?.uid?
+                              <>
+                             
+                              <button onClick={ handleLogOut} >Log out</button>
+                              </>
+                              :
+                              <>
+                              <Link className='btn btn-sm px-10 bg-transparent text-black border border-[#6F57E9] normal-case hover:bg-[#6F57E9] hover:text-white' to='/login'>Learn more</Link>
+                              
+                              </>
+                          }
+                          </div>
+              </div>
+    
+</React.Fragment>
+    return (
+        <div className="navbar md:top[-70] relative bg-gradient-to-r from-transparent to-indigo-600/10  font-bold flex justify-between">
+        <div className="navbar-start">
+        <div className="dropdown">
+                <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                </label>
+                <ul tabIndex={1} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-gray-100 rounded-box w-52">
+                    {menuItems}
+                </ul>
+            </div>
+            <Link to="/" className="normal-case text-xl "><span className=' font-light text-2xl lg:text-3xl font-serif'>Ship<span className='text-[#6F57E9]'>mate.</span></span></Link>
         </div>
-      </label>
-      <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
+        <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal  p-0">
+                {menuItems}
+            </ul>
+        </div>
+      
     </div>
-  </div>
-</div>
-    </div>
-  )
-}
+    );
+};
 
-export default Header
+export default Header;
